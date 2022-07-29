5d1a2c0f741c06d2be8c92465b7ede54fb2b72c9
Hara <harachung90@gmail.com>
2022-07-29 21:07:07 +1000

    Second commit. Step 9 - 15. Complete!

--- src/PrimeDirective.java
+++ src/PrimeDirective.java
@@ -3,7 +3,6 @@ import java.util.ArrayList;
 
 public class PrimeDirective {
 
-    // Add your methods here:
     public boolean isPrime(int number){
         if (number == 2) {
             return true;
@@ -21,14 +20,30 @@ public class PrimeDirective {
         return true;
     }
 
+    public ArrayList<Integer> onlyPrimes(int[] numbers) {
+        ArrayList<Integer> primes = new ArrayList<Integer>();
 
+        for (int number: numbers) {
+            if (isPrime(number)) {
+                primes.add(number);
+            }
+        }
+        return primes;
+    }
 
     public static void main(String[] args) {
 
         PrimeDirective pd = new PrimeDirective();
         int[] numbers = {6, 29, 28, 33, 11, 100, 101, 43, 89};
+
+        // should print true
         System.out.println(pd.isPrime(7));
+
+        // should print false
         System.out.println(pd.isPrime(28));
+
+        // should print [29, 11, 101, 43, 89]
+        System.out.println(pd.onlyPrimes(numbers));
 
     }
 
